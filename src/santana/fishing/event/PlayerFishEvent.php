c6cd5e98e7223c53eeb572b34405c9faa13a3888
kostamax27 <69608272+kostamax27@users.noreply.github.com>
2024-10-31 01:31:40 +0300

    Fix codestyle & Cleanup

--- src/santana/fishing/event/PlayerFishEvent.php
+++ src/santana/fishing/event/PlayerFishEvent.php
@@ -10,105 +10,53 @@ use pocketmine\event\player\PlayerEvent;
 use pocketmine\item\Item;
 use pocketmine\player\Player;
 
-final class PlayerFishEvent extends PlayerEvent implements Cancellable
-{
-    use CancellableTrait;
+final class PlayerFishEvent extends PlayerEvent implements Cancellable{
+	use CancellableTrait;
 
-    /**
-     * @var Player
-     */
-    protected $player;
+	protected $player;
 
-    /**
-     * @var Item
-     */
-    protected Item $fishingRod;
+	protected Item $fishingRod;
 
-    /**
-     * @var Item
-     */
-    protected Item $loot;
+	protected Item $loot;
 
-    /**
-     * @var int
-     */
-    protected int $experience;
+	protected int $experience;
 
-    /**
-     * @param Player $player
-     * @param Item $fishingRod
-     * @param Item $loot
-     * @param int $experience
-     */
-    public function __construct(Player $player, Item $fishingRod, Item $loot, int $experience)
-    {
-        $this->player = $player;
-        $this->fishingRod = $fishingRod;
-        $this->loot = $loot;
-        $this->experience = $experience;
-    }
+	public function __construct(Player $player, Item $fishingRod, Item $loot, int $experience){
+		$this->player = $player;
+		$this->fishingRod = $fishingRod;
+		$this->loot = $loot;
+		$this->experience = $experience;
+	}
 
-    /**
-     * @return Player
-     */
-    public function getPlayer(): Player
-    {
-        return $this->player;
-    }
+	public function getPlayer() : Player{
+		return $this->player;
+	}
 
-    /**
-     * @param Player $player
-     */
-    public function setPlayer(Player $player): void
-    {
-        $this->player = $player;
-    }
+	public function setPlayer(Player $player) : void{
+		$this->player = $player;
+	}
 
-    /**
-     * @return Item
-     */
-    public function getFishingRod(): Item
-    {
-        return $this->fishingRod;
-    }
+	public function getFishingRod() : Item{
+		return $this->fishingRod;
+	}
 
-    /**
-     * @param Item $fishingRod
-     */
-    public function setFishingRod(Item $fishingRod): void
-    {
-        $this->fishingRod = $fishingRod;
-    }
+	public function setFishingRod(Item $fishingRod) : void{
+		$this->fishingRod = $fishingRod;
+	}
 
-    /**
-     * @return Item
-     */
-    public function getLoot(): Item
-    {
-        return $this->loot;
-    }
+	public function getLoot() : Item{
+		return $this->loot;
+	}
 
-    /**
-     * @param Item $loot
-     */
-    public function setLoot(Item $loot): void
-    {
-        $this->loot = $loot;
-    }
+	public function setLoot(Item $loot) : void{
+		$this->loot = $loot;
+	}
 
-    /**
-     * @return int
-     */
-    public function getExperience(): int
-    {
-        return $this->experience;
-    }
+	public function getExperience() : int{
+		return $this->experience;
+	}
 
-    /**
-     * @param int $experience
-     */
-    public function setExperience(int $experience): void
-    {
-        $this->experience = $experience;
-    }
+	public function setExperience(int $experience) : void{
+		$this->experience = $experience;
+	}
 }
